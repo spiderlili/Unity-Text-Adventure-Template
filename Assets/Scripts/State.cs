e970c7d387cd013a6672769ce76519a1777ccd89
Jing Tan <jtan1993@gmail.com>
2018-06-16 09:47:26 +0100

    a

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject {
+
+    //min size, amount of lines before starting to scroll
+    [TextArea(14, 10)][SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+    
+
+    public string GetStateStory() {
+        return storyText;
+    }
+
+    public State[] GetNextStates() {
+        return nextStates;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/TextAdventure.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAdventure : MonoBehaviour {
+
+    //available within inspector
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+    State currentState;
+
+    // Use this for initialization
+    void Start () {
+        currentState = startingState;
+        textComponent.text = currentState.GetStateStory();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ManageState();
+	}
+
+    private void ManageState()
+    {
+        State[] NextStates = currentState.GetNextStates();
+
+        //If option 1 is selected
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentState = NextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentState = NextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentState = NextStates[2];
+        }
+        textComponent.text = currentState.GetStateStory();
+    }
+}
